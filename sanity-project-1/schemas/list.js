5ec8dd47fa21553ad1bc6df461be5466540a6e42
Patrick <patrick@meetpatrick.dev>
2022-11-29 16:17:37 -0600

    schema update

---
+++ sanity-project-1/schemas/list.js
@@ -0,0 +1,14 @@
+import card from './card'
+
+export default {
+    name: 'list',
+    type: 'document',
+    title: 'A List',
+    fields: [
+        {    
+            title: 'Product Card',
+            name: 'card',
+            type: 'card'
+        }
+    ]
+}
